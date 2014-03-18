3cdfdd2cff5fc3ceee74c8129bc75d990dbb3ea8
Vlad Agiyevich <sky-mart@hotmail.com>
2014-03-18 19:43:38 +0400

    Примерный контроллер и вьюшка.

--- Boobies/Boobies/BoobiesViewController.m
+++ Boobies/Boobies/BoobiesViewController.m
@@ -7,23 +7,60 @@
 //
 
 #import "BoobiesViewController.h"
+#include "Deck.h"
+#include "CardMatchingGame.h"
 
 @interface BoobiesViewController ()
+@property (strong, nonatomic) Deck *deck;
+@property (strong, nonatomic) CardMatchingGame *game;
 
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @end
 
 @implementation BoobiesViewController
 
-- (void)viewDidLoad
+- (CardMatchingGame *)game
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    if (!_game) {
+        _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
+                                                  usingDeck:[self createDeck]];
+    }
+    return _game;
 }
 
-- (void)didReceiveMemoryWarning
+- (Deck *)deck
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    if (!_deck) _deck = [self createDeck];
+    return _deck;
+}
+
+- (Deck *)createDeck //abstract
+{
+    return nil;
+}
+
+- (IBAction)touchCardButton:(UIButton *)sender
+{
+    NSUInteger cardIndex = [self.cardButtons indexOfObject:sender];
+    [self.game chooseCardAtIndex:cardIndex];
+    [self updateUI];
+}
+
+- (void)updateUI
+{
+    for (int i = 0; i < [self.cardButtons count]; i++) {
+        UIButton *cardButton = self.cardButtons[i];
+        Card *card = [self.game cardAtIndex:i];
+        [cardButton setBackgroundImage:[self backgroundImageForCard:card]
+                              forState:UIControlStateNormal];
+        cardButton.enabled = !card.matched;
+    }
+}
+
+
+- (UIImage *)backgroundImageForCard:(Card *)card
+{
+    return [UIImage imageNamed:card.isChosen ? @"cardfront" : @"cardback"];
 }
 
 @end
